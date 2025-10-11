7e69a0fff6c73a534cd3a98d0f7aebbf5f4f6b82
Chong Tze Yan <tze088@gmail.com>
2025-10-12 01:43:35 +0800

    Reduce duplicated code in ParseUtil Add parseDescription method

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -5,10 +5,13 @@ import static java.util.Objects.requireNonNull;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.util.StringUtil;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.event.Description;
 import seedu.address.model.group.GroupName;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
@@ -42,12 +45,7 @@ public class ParserUtil {
      * @throws ParseException if the given {@code name} is invalid.
      */
     public static Name parseName(String name) throws ParseException {
-        requireNonNull(name);
-        String trimmedName = name.trim();
-        if (!Name.isValidName(trimmedName)) {
-            throw new ParseException(Name.MESSAGE_CONSTRAINTS);
-        }
-        return new Name(trimmedName);
+        return parseField(name, Name::isValidName, Name.MESSAGE_CONSTRAINTS, Name::new);
     }
 
     /**
@@ -57,12 +55,7 @@ public class ParserUtil {
      * @throws ParseException if the given {@code phone} is invalid.
      */
     public static Phone parsePhone(String phone) throws ParseException {
-        requireNonNull(phone);
-        String trimmedPhone = phone.trim();
-        if (!Phone.isValidPhone(trimmedPhone)) {
-            throw new ParseException(Phone.MESSAGE_CONSTRAINTS);
-        }
-        return new Phone(trimmedPhone);
+        return parseField(phone, Phone::isValidPhone, Phone.MESSAGE_CONSTRAINTS, Phone::new);
     }
 
     /**
@@ -72,12 +65,28 @@ public class ParserUtil {
      * @throws ParseException if the given {@code email} is invalid.
      */
     public static Email parseEmail(String email) throws ParseException {
-        requireNonNull(email);
-        String trimmedEmail = email.trim();
-        if (!Email.isValidEmail(trimmedEmail)) {
-            throw new ParseException(Email.MESSAGE_CONSTRAINTS);
-        }
-        return new Email(trimmedEmail);
+        return parseField(email, Email::isValidEmail, Email.MESSAGE_CONSTRAINTS, Email::new);
+    }
+
+    /**
+     * Parses a {@code String name} into a {@code GroupName}.
+     * Leading and trailing whitespaces will be trimmed.
+     *
+     * @throws ParseException if the given {@code name} is invalid.
+     */
+    public static GroupName parseGroupName(String name) throws ParseException {
+        return parseField(name, GroupName::isValidName, GroupName.MESSAGE_CONSTRAINTS, GroupName::new);
+    }
+
+    /**
+     * Parses a {@code String description} into a {@code Description}.
+     * Leading and trailing whitespaces will be trimmed.
+     *
+     * @throws ParseException if the given {@code description} is invalid.
+     */
+    public static Description parseDescription(String description) throws ParseException {
+        return parseField(description, Description::isValidDescription,
+                Description.MESSAGE_CONSTRAINTS, Description::new);
     }
 
     /**
@@ -87,12 +96,7 @@ public class ParserUtil {
      * @throws ParseException if the given {@code tag} is invalid.
      */
     public static Tag parseTag(String tag) throws ParseException {
-        requireNonNull(tag);
-        String trimmedTag = tag.trim();
-        if (!Tag.isValidTagName(trimmedTag)) {
-            throw new ParseException(Tag.MESSAGE_CONSTRAINTS);
-        }
-        return new Tag(trimmedTag);
+        return parseField(tag, Tag::isValidTagName, Tag.MESSAGE_CONSTRAINTS, Tag::new);
     }
 
     /**
@@ -108,17 +112,19 @@ public class ParserUtil {
     }
 
     /**
-     * Parses a {@code String name} into a {@code GroupName}.
-     * Leading and trailing whitespaces will be trimmed.
-     *
-     * @throws ParseException if the given {@code name} is invalid.
+     * Generic method for parsing validated field types.
+     * @param input The String to be parsed.
+     * @param validator T.isValidT.
+     * @param message T.MESSAGE_CONSTRAINTS.
+     * @param constructor The constructor for T.
      */
-    public static GroupName parseGroupName(String name) throws ParseException {
-        requireNonNull(name);
-        String trimmedName = name.trim();
-        if (!GroupName.isValidName(trimmedName)) {
-            throw new ParseException(GroupName.MESSAGE_CONSTRAINTS);
+    private static <T> T parseField(String input, Predicate<String> validator, String message,
+            Function<String, T> constructor) throws ParseException {
+        requireNonNull(input);
+        String trimmedInput = input.trim();
+        if (!validator.test(trimmedInput)) {
+            throw new ParseException(message);
         }
-        return new GroupName(trimmedName);
+        return constructor.apply(trimmedInput);
     }
 }
