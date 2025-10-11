3b4e1ef6da3fc78b88fbcc30887177f0f073686b
Chong Tze Yan <tze088@gmail.com>
2025-10-12 03:10:43 +0800

    Add valid description constants to DescriptionTest
    We can use these in testing.
    Definition of a valid description can be done within a single file.

--- src/test/java/seedu/address/model/event/DescriptionTest.java
+++ src/test/java/seedu/address/model/event/DescriptionTest.java
@@ -1,6 +1,8 @@
 package seedu.address.model.event;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
@@ -8,6 +10,16 @@ import org.junit.jupiter.api.Test;
 
 
 public class DescriptionTest {
+
+    public static final String VALID_DESCRIPTION_STRING = "Weekly report";
+    public static final Description VALID_DESCRIPTION = new Description(VALID_DESCRIPTION_STRING);
+    public static final Description VALID_DESCRIPTION_2 = new Description("Project meeting");
+
+    @Test
+    void constructor_validInput() {
+        assertNotNull(VALID_DESCRIPTION);
+        assertEquals(VALID_DESCRIPTION_STRING, VALID_DESCRIPTION.description());
+    }
 
     @Test
     public void constructor_null_throwsNullPointerException() {
@@ -30,20 +42,20 @@ public class DescriptionTest {
         assertFalse(Description.isValidDescription(" ")); // spaces only
 
         // valid descriptions
+        assertTrue(Description.isValidDescription(VALID_DESCRIPTION_STRING));
         assertTrue(Description.isValidDescription("valid description")); // alphabets only
         assertTrue(Description.isValidDescription("12345")); // numbers only
         assertTrue(Description.isValidDescription("A description with symbols!@#")); // alphanumeric with symbols
         assertTrue(Description.isValidDescription("peter the 2nd")); // alphanumeric with spaces
-        assertTrue(Description.isValidDescription("Valid Description")); // with capital letters
-        assertTrue(Description.isValidDescription("A long description with many words that go on forever!"));
+        assertTrue(Description.isValidDescription("A long description with many words that go on forever"));
     }
 
     @Test
     public void equals() {
-        Description description = new Description("Valid description");
+        Description description = VALID_DESCRIPTION;
 
         // same values -> returns true
-        assertTrue(description.equals(new Description("Valid description")));
+        assertTrue(description.equals(new Description(VALID_DESCRIPTION_STRING)));
 
         // same object -> returns true
         assertTrue(description.equals(description));
@@ -55,6 +67,6 @@ public class DescriptionTest {
         assertFalse(description.equals(5.0f));
 
         // different values -> returns false
-        assertFalse(description.equals(new Description("Other valid description")));
+        assertFalse(description.equals(VALID_DESCRIPTION_2));
     }
 }
