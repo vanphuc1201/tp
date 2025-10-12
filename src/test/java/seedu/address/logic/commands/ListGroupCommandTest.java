666ab684202a163bc62ab47cca7c0b0eb5c98f9b
kentalim2 <kentalim2@gmail.com>
2025-10-13 06:04:50 +0800

    Change package name in test class

--- src/test/java/seedu/address/logic/commands/ListGroupCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListGroupCommandTest.java
@@ -8,7 +8,7 @@ import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_GROUP;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import seedu.address.logic.commands.groups.ListGroupCommand;
+import seedu.address.logic.commands.group.ListGroupCommand;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
