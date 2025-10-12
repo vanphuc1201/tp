5e341d90d7db0dfa9914574cc5029d1cb5dc4838
kentalim2 <kentalim2@gmail.com>
2025-10-13 04:07:20 +0800

    Fix minor checkstyle issue

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -16,8 +16,8 @@ import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.HelpCommand;
-import seedu.address.logic.commands.persons.AddCommand;
 import seedu.address.logic.commands.ClearCommand;
+import seedu.address.logic.commands.persons.AddCommand;
 import seedu.address.logic.commands.persons.DeleteCommand;
 import seedu.address.logic.commands.persons.EditCommand;
 import seedu.address.logic.commands.persons.EditCommand.EditPersonDescriptor;
