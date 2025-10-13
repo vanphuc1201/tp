632ab77ad0e7e68a98ee904997b40baf5fb61222
Carameow <iywudla@gmail.com>
2025-10-13 23:09:30 +0800

    Update AddGroupCommandParser.java

--- src/main/java/seedu/address/logic/parser/group/AddGroupCommandParser.java
+++ src/main/java/seedu/address/logic/parser/group/AddGroupCommandParser.java
@@ -10,6 +10,7 @@ import seedu.address.logic.parser.ArgumentMultimap;
 import seedu.address.logic.parser.ArgumentTokenizer;
 import seedu.address.logic.parser.Parser;
 import seedu.address.logic.parser.ParserUtil;
+import seedu.address.logic.parser.Prefix;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.event.UniqueEventList;
 import seedu.address.model.group.Group;
