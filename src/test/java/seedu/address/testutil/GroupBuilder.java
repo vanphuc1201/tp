05b5277def861df7608f4538c2dab078854be7d1
Chong Tze Yan <tze088@gmail.com>
2025-10-14 16:09:48 +0800

    Make GroupBuilder use factory method

--- src/test/java/seedu/address/testutil/GroupBuilder.java
+++ src/test/java/seedu/address/testutil/GroupBuilder.java
@@ -62,7 +62,7 @@ public class GroupBuilder {
     }
 
     public Group build() {
-        return new Group(name, events, persons);
+        return Group.fromStorage(name, events, persons);
     }
 
 }
