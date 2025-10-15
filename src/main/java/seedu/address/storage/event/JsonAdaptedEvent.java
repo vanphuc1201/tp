81d85678bbc31ab81e901e07e46f58a82ef44da0
Chong Tze Yan <tze088@gmail.com>
2025-10-15 12:57:34 +0800

    Add tests for JsonAdaptedEvent

--- src/main/java/seedu/address/storage/event/JsonAdaptedEvent.java
+++ src/main/java/seedu/address/storage/event/JsonAdaptedEvent.java
@@ -41,6 +41,9 @@ public class JsonAdaptedEvent {
             throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT,
                     Description.class.getSimpleName()));
         }
+        if (!Description.isValidDescription(description)) {
+            throw new IllegalValueException(Description.MESSAGE_CONSTRAINTS);
+        }
         final Description modelDescription = new Description(description);
 
         return new Event(modelDescription);
