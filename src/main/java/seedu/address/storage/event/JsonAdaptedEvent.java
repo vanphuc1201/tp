4bb5857cb7970c6fe10479b9d11cc07db2a06faa
Chong Tze Yan <tze088@gmail.com>
2025-10-15 12:15:45 +0800

    Add Json friendly Event class

---
+++ src/main/java/seedu/address/storage/event/JsonAdaptedEvent.java
@@ -0,0 +1,48 @@
+package seedu.address.storage.event;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.event.Description;
+import seedu.address.model.event.Event;
+
+/**
+ * Jackson-friendly version of {@link Event}.
+ */
+public class JsonAdaptedEvent {
+
+    public static final String MISSING_FIELD_MESSAGE_FORMAT = "Event's %s field is missing!";
+
+    private final String description;
+
+    /**
+     * Constructs a {@code JsonAdaptedEvent} with the given details
+     */
+    @JsonCreator
+    public JsonAdaptedEvent(@JsonProperty("description") String description) {
+        this.description = description;
+    }
+
+    /**
+     * Converts a given {@code Event} into this class for Jackson use.
+     */
+    public JsonAdaptedEvent(Event source) {
+        description = source.description().toString();
+    }
+
+    /**
+     * Converts this Jackson-friendly adapted event object into the model's {@code Event} object.
+     *
+     * @throws IllegalValueException if there were any data constraints violated in the adapted event.
+     */
+    public Event toModelType() throws IllegalValueException {
+        if (description == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT,
+                    Description.class.getSimpleName()));
+        }
+        final Description modelDescription = new Description(description);
+
+        return new Event(modelDescription);
+    }
+}
