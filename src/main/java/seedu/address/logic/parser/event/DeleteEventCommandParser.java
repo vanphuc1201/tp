1fd9d638ae35899a36f37550922222e890a262c1
Chong Tze Yan <tze088@gmail.com>
2025-10-23 15:43:34 +0800

    Implement DeleteEventCommandParser

---
+++ src/main/java/seedu/address/logic/parser/event/DeleteEventCommandParser.java
@@ -0,0 +1,45 @@
+package seedu.address.logic.parser.event;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EVENT_INDEX;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.event.DeleteEventCommand;
+import seedu.address.logic.parser.ArgumentMultimap;
+import seedu.address.logic.parser.ArgumentTokenizer;
+import seedu.address.logic.parser.Parser;
+import seedu.address.logic.parser.ParserUtil;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and creates a new DeleteEventCommand object.
+ */
+public class DeleteEventCommandParser implements Parser<DeleteEventCommand> {
+    /**
+     * Parses the given {@code String} of arguments in the context of the DeleteEventCommand
+     * and returns a DeleteEventCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public DeleteEventCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_EVENT_INDEX);
+
+        Index groupIndex;
+        Index eventIndex;
+
+        try {
+            groupIndex = ParserUtil.parseIndex(argMultimap.getPreamble());
+        } catch (ParseException pe) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    DeleteEventCommand.MESSAGE_USAGE), pe);
+        }
+        if (!ParserUtil.arePrefixesPresent(argMultimap, PREFIX_EVENT_INDEX)) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteEventCommand.MESSAGE_USAGE));
+        }
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_EVENT_INDEX);
+
+        eventIndex = ParserUtil.parseIndex(argMultimap.getValue(PREFIX_EVENT_INDEX).get());
+
+        return new DeleteEventCommand(groupIndex, eventIndex);
+    }
+}
