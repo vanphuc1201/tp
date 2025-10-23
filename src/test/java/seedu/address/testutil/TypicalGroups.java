2ea918d877ba8230181b0a12a140535f8beb4ca0
GuoGangQuan <e1232635@u.nus.edu>
2025-10-23 18:09:13 +0800

    Add withRepoLink to first group and sync the json storage

--- src/test/java/seedu/address/testutil/TypicalGroups.java
+++ src/test/java/seedu/address/testutil/TypicalGroups.java
@@ -1,5 +1,6 @@
 package seedu.address.testutil;
 
+import static seedu.address.model.group.RepoLinkTest.VALID_REPO;
 import static seedu.address.testutil.TypicalPersons.BENSON;
 import static seedu.address.testutil.TypicalPersons.CARL;
 import static seedu.address.testutil.TypicalPersons.DANIEL;
@@ -13,6 +14,7 @@ import java.util.List;
 
 import seedu.address.model.AddressBook;
 import seedu.address.model.group.Group;
+import seedu.address.model.group.RepoLink;
 import seedu.address.model.person.Person;
 
 /**
@@ -21,7 +23,7 @@ import seedu.address.model.person.Person;
 public class TypicalGroups {
 
     public static final Group CS2103T = new GroupBuilder().withName("CS2103T")
-            .withPersons(BENSON, DANIEL, CARL, FIONA)
+            .withPersons(BENSON, DANIEL, CARL, FIONA).withRepoLink(VALID_REPO)
             .build();
     public static final Group CS2101_CA2 = new GroupBuilder().withName("CS2101 CA2")
             .withPersons(CARL)
