7a963fcd2d33c7c0b3b9c5af188477b09f8a57da
GuoGangQuan <e1232635@u.nus.edu>
2025-10-23 22:43:55 +0800

    Fix error message for EditCommand

--- src/main/java/seedu/address/logic/commands/person/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/person/EditCommand.java
@@ -46,7 +46,7 @@ public class EditCommand extends Command {
             + "[" + PREFIX_GROUP_INDEX + "GROUP INDEX]\n"
             + "Example: " + COMMAND_WORD + " 1 "
             + PREFIX_PHONE + "91234567 "
-            + PREFIX_EMAIL + "johndoe@example.com"
+            + PREFIX_EMAIL + "johndoe@example.com "
             + PREFIX_GROUP_INDEX + "1";
 
     public static final String MESSAGE_EDIT_PERSON_SUCCESS = "Edited Person: %1$s";
