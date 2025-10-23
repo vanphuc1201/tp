df94db5a0623df2d84c124ae42737c1c55844299
GuoGangQuan <e1232635@u.nus.edu>
2025-10-23 18:54:20 +0800

    Add FakeClipboardUtil to simulates clipboard behavior.

--- src/test/java/seedu/address/logic/commands/group/GetRepoCommandTest.java
+++ src/test/java/seedu/address/logic/commands/group/GetRepoCommandTest.java
@@ -12,6 +12,7 @@ import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_GROUP;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.Index;
+import seedu.address.commons.util.ClipboardUtil;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
@@ -23,33 +24,39 @@ import seedu.address.model.group.Group;
 public class GetRepoCommandTest {
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
+    private FakeClipboardUtil fakeClipboardUtil = new FakeClipboardUtil();
+
     @Test
     public void execute_validIndex_success() throws CommandException {
         Group groupToRetrieve = model.getFilteredGroupList().get(INDEX_SECOND_GROUP.getZeroBased());
-        GetRepoCommand getRepoCommand = new GetRepoCommand(INDEX_SECOND_GROUP);
+        GetRepoCommand getRepoCommand = new GetRepoCommand(INDEX_SECOND_GROUP, fakeClipboardUtil);
         CommandResult result = getRepoCommand.execute(model);
 
         String expectedMessage = String.format(GetRepoCommand.MESSAGE_SUCCESS, groupToRetrieve.getName(), VALID_REPO);
 
         assertEquals(expectedMessage, result.getFeedbackToUser());
+        assertEquals(VALID_REPO.toString(), fakeClipboardUtil.copiedText);
     }
 
     @Test
     public void execute_inValidIndex_success() {
         GetRepoCommand getRepoCommand =
-                new GetRepoCommand(Index.fromZeroBased(model.getFilteredGroupList().size() + 1));
+                new GetRepoCommand(Index.fromZeroBased(model.getFilteredGroupList().size() + 1),
+                        fakeClipboardUtil);
 
         assertCommandFailure(getRepoCommand, model, Messages.MESSAGE_INVALID_GROUP_DISPLAYED_INDEX);
+        assertEquals(null, fakeClipboardUtil.copiedText);
     }
 
     @Test
     public void execute_defaultRepo_success() {
         Group groupToRetrieve = model.getFilteredGroupList().get(INDEX_FIRST_GROUP.getZeroBased());
-        GetRepoCommand getRepoCommand = new GetRepoCommand(INDEX_FIRST_GROUP);
+        GetRepoCommand getRepoCommand = new GetRepoCommand(INDEX_FIRST_GROUP, fakeClipboardUtil);
 
         String expectedMessage = String.format(GetRepoCommand.MESSAGE_GET_REPO_NOT_SET, groupToRetrieve.getName());
 
         assertCommandFailure(getRepoCommand, model, expectedMessage);
+        assertEquals(null, fakeClipboardUtil.copiedText);
     }
 
     @Test
@@ -83,4 +90,25 @@ public class GetRepoCommandTest {
         String expected = GetRepoCommand.class.getCanonicalName() + "{targetIndex=" + INDEX_FIRST_GROUP + "}";
         assertEquals(expected, command.toString());
     }
+
+    /**
+     * A test utility class that simulates clipboard behavior without accessing the system clipboard.
+     * <p>
+     * Used in unit tests to verify that text is correctly copied without causing
+     * {@link java.awt.HeadlessException} in headless environments.
+     */
+    public class FakeClipboardUtil extends ClipboardUtil {
+        private String copiedText = null;
+
+        @Override
+        public void copyToClipboard(String text) {
+            this.copiedText = text;
+        }
+
+        public String getCopiedText() {
+            return copiedText;
+        }
+    }
 }
+
+
