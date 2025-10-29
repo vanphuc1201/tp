3445a1e8924ef71751696d7f815d08171ae06f55
GuoGangQuan <e1232635@u.nus.edu>
2025-10-29 17:24:08 +0800

    Change Phone number constraint to min 8 digits max 15 digits

--- src/main/java/seedu/address/model/person/Phone.java
+++ src/main/java/seedu/address/model/person/Phone.java
@@ -11,8 +11,11 @@ public class Phone {
 
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Phone numbers should only contain numbers, and it should be at least 3 digits long";
-    public static final String VALIDATION_REGEX = "\\d{3,}";
+            "Phone numbers have following constraints:\n" +
+                    "- Should only contain numbers\n" +
+                    "- At least 8 digits\n" +
+                    "- Maximum 15 digits";
+    public static final String VALIDATION_REGEX = "^\\+?[0-9]{8,15}$";
     public final String value;
 
     /**
