70d072065967cf80f72711016f88429e12ae9219
GuoGangQuan <e1232635@u.nus.edu>
2025-10-29 17:27:34 +0800

    Fix test case error cause by new phone number constraint

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -32,19 +32,19 @@ public class TypicalPersons {
             .withEmail("heinz@example.com").withGroups("CS2103T", "CS2101 CA2", "IS1108").build();
     public static final Person DANIEL = new PersonBuilder().withName("Daniel Meier").withPhone("87652533")
             .withEmail("cornelia@example.com").withGroups("CS2103T", "CS2101 CA3", "IS1108").build();
-    public static final Person ELLE = new PersonBuilder().withName("Elle Meyer").withPhone("9482224")
+    public static final Person ELLE = new PersonBuilder().withName("Elle Meyer").withPhone("94822241")
             .withEmail("werner@example.com").withGroups("CS2101 CA3").build();
-    public static final Person FIONA = new PersonBuilder().withName("Fiona Kunz").withPhone("9482427")
+    public static final Person FIONA = new PersonBuilder().withName("Fiona Kunz").withPhone("94824271")
             .withEmail("lydia@example.com").withGroups("CS2103T", "CS2101 CA3", "IS1108").build();
-    public static final Person GEORGE = new PersonBuilder().withName("George Best").withPhone("9482442")
+    public static final Person GEORGE = new PersonBuilder().withName("George Best").withPhone("94824421")
             .withEmail("anna@example.com").build();
-    public static final Person GEORGE_WITH_GROUP = new PersonBuilder().withName("George Best").withPhone("9482442")
+    public static final Person GEORGE_WITH_GROUP = new PersonBuilder().withName("George Best").withPhone("94824421")
             .withEmail("anna@example.com").withGroups("CS2103T").build();
 
     // Manually added
-    public static final Person HOON = new PersonBuilder().withName("Hoon Meier").withPhone("8482424")
+    public static final Person HOON = new PersonBuilder().withName("Hoon Meier").withPhone("84824241")
             .withEmail("stefan@example.com").build();
-    public static final Person IDA = new PersonBuilder().withName("Ida Mueller").withPhone("8482131")
+    public static final Person IDA = new PersonBuilder().withName("Ida Mueller").withPhone("84821311")
             .withEmail("hans@example.com").build();
 
     // Manually added - Person's details found in {@code CommandTestUtil}
